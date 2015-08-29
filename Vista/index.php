9d6cadea089d112555d9acda59d302bb4cf3757c
ThePantherPink <19santiagodiaz94@gmail.com>
2015-08-29 19:55:59 -0300

    commit 4

--- Vista/index.php
+++ Vista/index.php
@@ -1,1 +1,20 @@
-<meta http-equiv="Refresh" content="1;url=desplazamientoIMG.html">
+<link rel="stylesheet" type="text/css" href="css/ampliar.css"/>
+<center>
+<br><br><br>
+<br><br><br>
+<br><br><br>
+
+
+<div id="ampliar">
+	<table>
+	<tr><td><img  src="imagenes/1.jpg" width="100" height="100"/></td>
+	<td><img  src="imagenes/2.jpg" width="100" height="100"/></td>
+	<td><img  src="imagenes/3.jpg" width="100" height="100"/></td></tr>
+	<tr><td><img  src="imagenes/1.jpg" width="100" height="100"/></td>
+	<td><img  src="imagenes/2.jpg" width="100" height="100"/></td>
+	<td><img  src="imagenes/3.jpg" width="100" height="100"/></td></tr>
+	</table>
+	</tr>
+</div>
+</center>
+<!-- <meta http-equiv="Refresh" content="1;url=desplazamientoIMG.html"> -->
