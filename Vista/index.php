b14d08c05ada7c3a3479389fc8ecceb599c88900
ThePantherPink <19santiagodiaz94@gmail.com>
2015-08-29 20:20:35 -0300

    commit 5

--- Vista/index.php
+++ Vista/index.php
@@ -1,4 +1,12 @@
-<link rel="stylesheet" type="text/css" href="css/ampliar.css"/>
+<!DOCTYPE html>
+<html>
+<head>
+    <script language="Javascript" type="text/javascript" src="js/jquery-2.1.3.js"></script>
+	<script language="Javascript" type="text/javascript" src="js/jquery.blinds-0.9.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/ampliar.css"/>
+</head>
+
+
 <center>
 <br><br><br>
 <br><br><br>
@@ -7,14 +15,19 @@
 
 <div id="ampliar">
 	<table>
-	<tr><td><img  src="imagenes/1.jpg" width="100" height="100"/></td>
-	<td><img  src="imagenes/2.jpg" width="100" height="100"/></td>
-	<td><img  src="imagenes/3.jpg" width="100" height="100"/></td></tr>
-	<tr><td><img  src="imagenes/1.jpg" width="100" height="100"/></td>
-	<td><img  src="imagenes/2.jpg" width="100" height="100"/></td>
-	<td><img  src="imagenes/3.jpg" width="100" height="100"/></td></tr>
+	<tr><td><img  src="imagenes/1.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td>
+	<td><img  src="imagenes/2.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td>
+	<td><img  src="imagenes/3.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td><td><img  src="imagenes/1.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td>
+	<td><img  src="imagenes/2.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td>
+	<td><img  src="imagenes/3.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td></tr>
+	<tr><td><img  src="imagenes/1.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td>
+	<td><img  src="imagenes/2.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td>
+	<td><img  src="imagenes/3.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td><td><img  src="imagenes/1.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td>
+	<td><img  src="imagenes/2.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td>
+	<td><img  src="imagenes/3.jpg" width="100" height="100" title="Mantenga apretado el click para ampliar la imagen"/></td></tr>
 	</table>
 	</tr>
 </div>
 </center>
+</html>
 <!-- <meta http-equiv="Refresh" content="1;url=desplazamientoIMG.html"> -->
